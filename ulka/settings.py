f2fe3c70b1d3d56f040c520151a565836a42feaf
thamshid <thamshid@qburst.com>
2018-07-13 02:09:11 +0530

    Requirement file updated

--- ulka/settings.py
+++ ulka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j(79p%)6!mj-kz-7o(330cw8$2a3_cxej^!%_%m1!2rbvowr!_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ulka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ulka_db',
+        'USER': 'ulka',
+        'PASSWORD': 'ulka',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
